e86b6e43ac6544690e699b6046679e4e0455f656
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 15:43:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  example5 = {
+    :key => value
+  }
 end
 
 def read_from_hash(hash, key)
